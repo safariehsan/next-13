b12cf0f1b6946dab5e2b4e2ef8cb5cad6c2b2177
Ehsan Safari <ehsansafari@hotmail.com>
2023-10-07 11:35:35 +0200

    implement dynamic routing: slug-searchParams,...

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+interface IUser {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  }
+
+const UserTable = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users = await res.json();
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-zebra">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item: IUser) => (
+            <tr key={item.id}>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12"></div>
+                  </div>
+                  <div>
+                    <div className="font-bold">{item.name}</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTable;
