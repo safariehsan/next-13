5ed7358e1ac4736d2bef747c969217f32938a26f
Ehsan Safari <ehsansafari@hotmail.com>
2023-10-08 19:22:33 +0200

    use prisma to CRUD on user entity

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,15 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import schema from "./schema";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Esi" },
-    { id: 2, name: "Eli" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json({ error: "User already exist!" }, { status: 400 });
+  }
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(createdUser, { status: 201 });
 }
