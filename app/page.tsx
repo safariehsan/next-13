7f2c3900619627fbcf9e611518acff0e5ddaabcf
Ehsan Safari <ehsansafari@hotmail.com>
2023-10-15 16:30:22 +0200

    image optimization + ga script

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import bgImg from "@/public/images/bg.jpg";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -10,6 +12,18 @@ export default async function Home() {
       <h1>Hello {session?.user!.name}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <Image src={bgImg} alt="bgImg" />
+      <Image
+        src="https://ehsansafari.com/bg.jpg"
+        width={800}
+        height={600}
+        alt="bg-img-remote-url"
+        fill
+        style={{ objectFit: "cover" }}
+        sizes="(max-width: 480) 100vw, (max-width: 768) 50vw, 33vw"
+        quality={75}
+        priority
+      />
     </main>
   );
 }
